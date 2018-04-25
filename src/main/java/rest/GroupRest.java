f9b3eb43242e314a96e826ac8884c23f508371ef
ilijagrbic <ilijagrb.ns@gmail.com>
2018-04-25 21:46:51 +0200

    Metoda za kreiranje i brisanje grupe otkucano.

--- src/main/java/rest/GroupRest.java
+++ src/main/java/rest/GroupRest.java
@@ -3,12 +3,15 @@ package rest;
 import dbClasses.GroupDatabase;
 import model.Group;
 import model.User;
+import org.bson.Document;
+import rest.dto.ErrorDTO;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @LocalBean
 @Stateless
@@ -29,16 +32,41 @@ public class GroupRest implements GroupRestRemote {
     @Path("/group")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Group createGroup(Group toCreate) {
-        return null;
+    public Response createGroup(Group toCreate) {
+        Document searchBy = new Document();
+        searchBy.put("id", toCreate.getId());
+
+        Document found = (Document)groupDatabase.getCollection().find(searchBy).first();
+        if(found != null){
+            return Response.status(Response.Status.CONFLICT).entity(new ErrorDTO("Group already exists.")).build();
+        }else{
+            groupDatabase.getCollection().insertOne(toCreate);
+
+            //TODO notify group participants about group creation
+
+            return Response.status(Response.Status.OK).entity(toCreate).build();
+        }
     }
 
     @DELETE
     @Path("/group")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Group deleteGroup(Group toDelete) {
-        return null;
+    public Response deleteGroup(Group toDelete) {
+        Document searchBy = new Document();
+        searchBy.put("id", toDelete.getId());
+
+        Document found = (Document)groupDatabase.getCollection().find(searchBy).first();
+        if(found == null){
+            return Response.status(Response.Status.NOT_FOUND).entity(new ErrorDTO("Group not found.")).build();
+        }else{
+            groupDatabase.getCollection().deleteOne(found);
+
+            //TODO notify group participants about group disbandment
+
+            return Response.status(Response.Status.OK).entity(toDelete).build();
+        }
+
     }
 
     @POST
