f5b37183ec9ed52b1eb9e4168694b764af21e7f6
ilijagrbic <ilijagrb.ns@gmail.com>
2018-04-25 22:02:58 +0200

    Delete usera iz grupe.

--- src/main/java/rest/GroupRest.java
+++ src/main/java/rest/GroupRest.java
@@ -89,8 +89,24 @@ public class GroupRest implements GroupRestRemote {
     @Path("/group/{groupId}/users/{userId}")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Group removeUser(@PathParam("groupId") String groupId, @PathParam("userId") String userId) {
-        return null;
+    public Response removeUser(@PathParam("groupId") String groupId, @PathParam("userId") String userId) {
+        Document searchBy = new Document();
+        searchBy.put("id", groupId);
+
+        Document found = (Document) groupDatabase.getCollection().find(searchBy).first();
+
+        if(found==null){
+            return Response.status(Response.Status.NOT_FOUND).entity(new ErrorDTO("Group not found.")).build();
+        }else{
+            groupDatabase.getCollection().updateOne(
+                    new Document(),
+                    new Document("$pull", new Document("id", new Document("$in", userId)))
+            );
+
+            //TODO notify other users about user that left
+
+            return Response.status(Response.Status.OK).entity(found).build();//check if u need to return found or someting else
+        }
     }
 
 
